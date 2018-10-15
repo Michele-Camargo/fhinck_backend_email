8a76697481e36a95f20a6596c1ae4358cde4ce1a
Michele de Camargo <mi.farias98@gmail.com>
2018-10-14 23:13:12 -0300

    Backend para envio de email e readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # fhink_backend_email
 Back-end do desafio da Fhink, responsável por enviar um email ao destinatário
+
+#### Comando para executar a aplicação
+```
+npm start
+```
--- source/index.js
+++ source/index.js
@@ -3,7 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer');
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.send("")
@@ -11,9 +11,35 @@ app.get("/", (req, res) => {
 
 app.post('/sendEmail', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1>Parabens</h1>')
+
+    const transporter = nodemailer.createTransport({
+        service: 'Hotmail',
+        auth: {
+            user: 'michele.desafio.fhink@hotmail.com',
+            pass: 'fhinkdesafio123'
+        }
+    });
+
+
+    let mailOptions = {
+        to: req.body.email,
+        subject: req.body.subject,
+        text: req.body.message + '\n\nEnviado de : ' + req.body.nome
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            resp.send('<h1>Não foi possivel enviar o email, tente novamente.</h1>')
+            return console.log(error);
+        } else {
+            resp.send('<h1>Email enviado com sucesso! Vamos trabahar nisso!</h1>')
+        }
+
+        console.log('Message sent: %s', info.messageId);
+
+    })
 })
 
-app.listen(3004, () =>{
+app.listen(3004, () => {
     console.log(`Servidor executando na porta ${3004}.`)
 })
